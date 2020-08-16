4170c637a21a68a6e73082c02509858161c16d2c
george_kpb <georgiy.kuznetsov.19@mail.ru>
2020-08-17 01:06:14 +0600

    Установка webpack, процесс запуска завершается ошибкой

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = reqiure('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist'),
+    },
+}
